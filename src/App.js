546859bf926a4d30a099fa0c38b6bebbc248ddcb
john <545244819@qq.com>
2018-05-02 13:57:04 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+import './App.css'
+
+class App extends Component {
+  render() {
+    const name = 'world'
+
+    return (
+      <div>{ name }</div>
+    )
+  }
+}
+
+export default App
