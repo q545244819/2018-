9206342c6519d1a3026253c80a4fcc461a58c9e3
john <545244819@qq.com>
2018-05-05 15:08:14 +0800

    feat: 完善首页数据结构

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -5,6 +5,36 @@ import { Pagination, Row, Col, Divider, Badge, Breadcrumb, Icon } from 'antd'
 import './index.css'
 
 class Home extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      list: [
+        {
+          id: '123',
+          title: '饿了么的 PWA 升级实践',
+          cover: 'http://placehold.it/300x200',
+          detail: '很荣幸在今年 2 月到 5 月的时间里，以顾问的身份加入饿了么，参与 PWA 的相关工作。这篇文章其实最初是在以英文写作发表在 medium 上的：Upgrading Ele.me to Progressive Web Apps，获得了一定的关注。所以也决定改写为中文版本再次分享出来，希望能对你有所帮助 ;) ',
+          date: '2018-01-01',
+        },
+      ],
+      tags: [
+        {
+          id: '123',
+          title: '粤菜',
+        },
+        {
+          id: '321',
+          title: '川菜',
+        },
+      ],
+      tagsMap: {
+        '123': '粤菜',
+        '321': '川菜',
+      }
+    }
+  }
+
   render() {
     return (
       <div className="Home">
@@ -13,17 +43,21 @@ class Home extends Component {
             <Col span={18}>
               <Breadcrumb>
                 <Breadcrumb.Item><Icon type="home" />首页</Breadcrumb.Item>
-                { this.props.match.params.id ? <Breadcrumb.Item><a href={`${this.props.match.params.id}`}>test</a></Breadcrumb.Item> : null }
+                { this.props.match.params.id ? <Breadcrumb.Item><a href={`${this.props.match.params.id}`}>{ this.state.tagsMap[this.props.match.params.id] }</a></Breadcrumb.Item> : null }
               </Breadcrumb>
               <ul className="list">
-                <li>
-                  <Link to="/">
-                    <h1>饿了么的 PWA 升级实践</h1>
-                    <img className="cover" src="http://placehold.it/300x200" alt="饿了么的 PWA 升级实践" />
-                    <p className="detail">很荣幸在今年 2 月到 5 月的时间里，以顾问的身份加入饿了么，参与 PWA 的相关工作。这篇文章其实最初是在以英文写作发表在 medium 上的：Upgrading Ele.me to Progressive Web Apps，获得了一定的关注。所以也决定改写为中文版本再次分享出来，希望能对你有所帮助 ;) </p>
-                    <p className="info">Posted by XXX on 2018-01-01</p>
-                  </Link>
-                </li>
+                { this.state.list.map((item, index) => {
+                  return (
+                    <li key={ item.id }>
+                      <Link to={`/menu/${ item.id }`}>
+                        <h1>{ item.title }</h1>
+                        <img className="cover" src={ item.cover } alt={ item.title } />
+                        <p className="detail">{ item.detail }</p>
+                        <p className="info">Posted by XXX on { item.date }</p>
+                      </Link>
+                    </li>
+                  )
+                }) }
               </ul>
               <Pagination defaultCurrent={1} total={50} />
             </Col>
@@ -31,7 +65,9 @@ class Home extends Component {
               <Divider />
               <h2>美食标签</h2>
               <div className="tags-wrap">
-                <Link to="/123"><Badge count="粤菜" /></Link>
+                { this.state.tags.map((item, index) => {
+                  return <Link key={ item.id } to={`/${ item.id }`}><Badge count={ item.title } /></Link>
+                }) }
               </div>
               <Divider />
               <h2>站长</h2>
